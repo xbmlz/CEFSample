72b035bca93b93d4441b5beb4e5cc0a7c1355ec0
997909544@qq.com <cxc19941217>
2020-03-21 21:27:48 +0800

    add winform cef demo

---
+++ WinFormSample/WinFormSample/CefKeyBoardHander.cs
@@ -0,0 +1,39 @@
+﻿using CefSharp;
+using System;
+using System.Windows.Forms;
+
+namespace WinFormSample
+{
+    public class CefKeyBoardHander : IKeyboardHandler
+    {
+        public bool OnKeyEvent(IWebBrowser browserControl, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey)
+        {
+            if (type == KeyType.KeyUp && Enum.IsDefined(typeof(Keys), windowsKeyCode))
+            {
+                var key = (Keys)windowsKeyCode;
+                switch (key)
+                {
+                    case Keys.F12:
+                        browser.ShowDevTools();
+                        break;
+                    case Keys.F5:
+                        if (modifiers == CefEventFlags.ControlDown)
+                        {
+                            browser.Reload(true); //强制忽略缓存
+                        }
+                        else
+                        {
+                            browser.Reload();
+                        }
+                        break;
+                }
+            }
+            return false;
+        }
+
+        public bool OnPreKeyEvent(IWebBrowser browserControl, IBrowser browser, KeyType type, int windowsKeyCode, int nativeKeyCode, CefEventFlags modifiers, bool isSystemKey, ref bool isKeyboardShortcut)
+        {
+            return false;
+        }
+    }
+}
---
+++ WinFormSample/WinFormSample/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using CefSharp;
+using CefSharp.WinForms;
+using System.Windows.Forms;
+
+namespace WinFormSample
+{
+    public partial class MainForm : Form
+    {
+        public ChromiumWebBrowser browser;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // 窗体关闭时退出CEF
+            browser.Dispose();
+            browser = null;
+            Cef.Shutdown();
+        }
+
+        private void MainForm_Load(object sender, System.EventArgs e)
+        {
+            InitializeBrowser();
+            WindowState = FormWindowState.Maximized;
+        }
+
+        // 初始化CEF
+        public void InitializeBrowser()
+        {
+            var settings = new CefSettings
+            {
+                Locale = "zh-CN"
+            };
+            Cef.EnableHighDPISupport();
+            Cef.Initialize(settings);
+            browser = new ChromiumWebBrowser("https://www.baidu.com/")
+            {
+                // 填充整个父组件
+                Dock = DockStyle.Fill,
+                // 按键响应
+                KeyboardHandler = new CefKeyBoardHander()
+            };
+            Controls.Add(browser);
+        }
+    }
+}
